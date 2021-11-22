d360651fb42a0c5df0a3586e7b45e99719286de4
Aditya Singh <aditya0930singh@gmail.com>
2021-11-22 15:27:31 +0530

    added a new bg image

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -30,7 +30,7 @@ export default {
 }
 
 .parallax {
-  background-image: url("../assets/img/bg.jpg");
+  background-image: url("../assets/img/fancy-pants.jpg");
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
