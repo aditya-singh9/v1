b7e997af9639f7dd83544c3b9603ebbcd2387419
Aditya Singh <aditya0930singh@gmail.com>
2021-12-21 09:14:14 +0530

    fix

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -2,11 +2,6 @@
   <div class="social-wrap">
     <ul>
       <li>
-        <a :href="links.metadata.twitter" target="_blank">
-          <font-awesome-icon class="icon" :icon="['fab', 'instagram-square']" />
-        </a>
-      </li>
-      <li>
         <a :href="links.metadata.linkedin" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
         </a>
